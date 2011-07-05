032f70129a78517056d2ba5f3f01d8f90e92a311
Peter Friese <peter@peterfriese.de>
2011-07-05 15:18:58 +0200

    Added drill down questions.

---
+++ Code/DrillDownQuestion.h
@@ -0,0 +1,13 @@
+//
+//  DrillDownQuestion.h
+//  IQuestionaire
+//
+//  Created by Peter Friese on 7/5/11.
+//  Copyright 2011 Peter Friese. All rights reserved.
+//
+
+#import "Question.h"
+
+@interface DrillDownQuestion : Question
+
+@end
---
+++ Code/DrillDownQuestion.m
@@ -0,0 +1,23 @@
+//
+//  DrillDownQuestion.m
+//  IQuestionaire
+//
+//  Created by Peter Friese on 7/5/11.
+//  Copyright 2011 Peter Friese. All rights reserved.
+//
+
+#import "DrillDownQuestion.h"
+
+@implementation DrillDownQuestion
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
